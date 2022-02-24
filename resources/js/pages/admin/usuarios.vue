962e53469935ac19b1ed4f0d39ff3470ec72ebdc
oriol <orioltunino1234@gmail.com>
2022-02-24 15:35:16 -0500

    diseño cards tramites

--- resources/js/pages/admin/usuarios.vue
+++ resources/js/pages/admin/usuarios.vue
@@ -1,33 +1,49 @@
+
+
 <template>
+
+<div>
+  <div>
+  <v-btn color="teal" class="ml-10 mt-5">
+    Agregar Tramite
+  </v-btn>
+  </div>
  <div 
   style="display:flex; flex-wrap:wrap; justify-content: space-evenly;"
  >
  
-      <v-card v-for="i in 10"
+      <v-card v-for="(proceso,i) in procesos" :key="i"
     class="mt-5 mb-5 ml-1 mr-1 "    
-    max-width="400"
-    min-width="200"   
+    width="420"
+     
   >
   
     <v-card-text >     
-      <p class="text-h4 text--primary">
-        BACHILLER AUTOMATICO VIRTUAL 
+      <p class="text-h6 indigo--text " >
+        {{proceso.procNom}}
       </p>
-        <div class="text--primary">
-        Fases :<strong>15</strong><br>
-        Rquisitos :<strong>10</strong>
+        <div class="text--primary "  >
+        Fases :<strong>15</strong> <br/>
+        Requisitos :<strong>10</strong>
       </div>
     </v-card-text>
     <v-card-actions>
-      <v-btn
-        text
-        color="deep-purple accent-4"
+      <v-btn  
+          
+        color="teal"
+        tile
+        outlined
+        elevation="2"
+     
       >
         Abrir
       </v-btn>
       <v-btn
-        text
-        color="deep-purple accent-4"
+        
+        tile
+        color="blue-grey"
+        outlined
+       elevation="2"
       >
         Desactivar
       </v-btn>
@@ -37,10 +53,36 @@
 
  
  </div>
-  
+</div>
 </template>
 
 <script>
+import axios from 'axios'
+   export default {
+    data () {
+      return {
+          procesos:[],
+       
+      }
+    },
+      mounted(){
+      this.FetchProceso();
+    }, 
+    
+    methods:{
+      async FetchProceso() {
+        const { data } = await axios.get("/api/proceso");
+        this.procesos = data;
 
+        //console.log(data);
+      },
+
+
+    
+    },
+  }
 </script>
 
+<style >
+ 
+</style>
