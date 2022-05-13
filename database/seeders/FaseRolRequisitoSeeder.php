3eb49ed3617bc2fd413a23ac9fb963fd6c2e3763
@calav4s4s <calav4s4s@gmail.com>
2022-05-13 17:50:59 -0500

    fases y requisitos de tramites

--- database/seeders/FaseRolRequisitoSeeder.php
+++ database/seeders/FaseRolRequisitoSeeder.php
@@ -44,8 +44,566 @@ class FaseRolRequisitoSeeder extends Seeder
                 'requisito_id'=>'1005',
                 'fase_id'=>'101',
             ],
-            
-            
+            [
+                'id'=>'6',
+                'rol_id'=>'10',
+                'fase_id' => '101',
+                'requisito_id' => '1006',
+            ],
+            [
+                'id'=>'7',
+                'rol_id'=>'10',
+                'fase_id' => '101',
+                'requisito_id' => '1007',
+            ],
+            [
+                'id'=>'8',
+                'rol_id'=>'10',
+                'fase_id' => '101',
+                'requisito_id' => '1008',
+            ],
+            [
+                'id'=>'9',
+                'rol_id'=>'10',
+                'fase_id' => '101',
+                'requisito_id' => '1009',
+
+            ],
+            [
+                'id'=>'10',
+                'rol_id'=>'10',
+                'fase_id' => '101',
+                'requisito_id' => '1010',
+
+            ],
+            [
+                'id'=>'11',
+                'rol_id'=>'10',
+                'fase_id' => '101',
+                'requisito_id' => '1011',
+            ],
+            [
+                'id'=>'12',
+                'rol_id'=>'10',
+                'fase_id' => '101',
+                'requisito_id' => '1012',
+            ],
+            [
+                'id'=>'13',
+                'rol_id'=>'10',
+                'fase_id' => '101',
+                'requisito_id' => '1013',
+            ],
+            [
+                'id'=>'14',
+                'rol_id'=>'10',
+                'fase_id' => '101',
+                'requisito_id' => '1014',
+            ],
+            [
+                'id'=>'15',
+                'rol_id'=>'10',
+                'fase_id' => '101',
+                'requisito_id' => '1015',
+
+            ],
+            [
+                'id'=>'16',
+                'rol_id'=>'10',
+                'fase_id' => '101',
+                'requisito_id' => '1016',
+
+            ],
+            [
+                'id'=>'17',
+                'rol_id'=>'10',
+                'fase_id' => '101',
+                'requisito_id' => '1017',
+            ],
+            [
+                'id'=>'18',
+                'rol_id'=>'10',
+                'fase_id' => '101',
+                'requisito_id' => '1018',
+            ],
+            [
+                'id'=>'19',
+                'rol_id'=>'10',
+                'fase_id' => '101',
+                'requisito_id' => '1019',
+            ],
+            [
+                'id'=>'20',
+                'rol_id'=>'10',
+                'fase_id' => '101',
+                'requisito_id' => '1020',
+            ],
+            [
+                'id'=>'21',
+                'rol_id'=>'10',
+                'fase_id' => '101',
+                'requisito_id' => '1021',
+            ],
+            [
+                'id'=>'22',
+                'rol_id'=>'10',
+                'fase_id' => '101',
+                'requisito_id' => '1022',
+            ],
+            //resolucion de expedito y consejo de facultad
+            [                
+                'id'=>'23',
+                'rol_id'=>'5',
+                'fase_id' => '102',
+                'requisito_id' => '1023',
+            ],
+            [
+                'id'=>'24',
+                'rol_id'=>'5',
+                'fase_id' => '102',
+                'requisito_id' => '1024',
+            ],
+            [
+                'id'=>'25',
+                'rol_id'=>'5',
+                'fase_id' => '102',
+                'requisito_id' => '1025',
+            ],
+            //Vicerrectorado
+            [
+                'id'=>'26',
+                'rol_id'=>'11',
+                'fase_id' => '103',
+                'requisito_id' => '1026',
+            ],
+        //1 fin bachiller automatico undac 
+        
+        //4 Inicio Bachiller trabajo undac   
+            //requisitos basicos
+            [
+                'id'=>'27',
+                'rol_id'=>'10',
+                'fase_id' => '401',
+                'requisito_id' => '1001',
+            ],
+            [
+                'id'=>'28',
+                'rol_id'=>'10',
+                'fase_id' => '401',
+                'requisito_id' => '1002',
+            ],
+            [
+                'id'=>'29',
+                'rol_id'=>'10',
+                'fase_id' => '401',
+                'requisito_id' => '1003',
+            ],
+            [
+                'id'=>'30',
+                'rol_id'=>'10',
+                'fase_id' => '401',
+                'requisito_id' => '1004',
+            ],
+            [
+                'id'=>'31',
+                'rol_id'=>'10',
+                'fase_id' => '401',
+                'requisito_id' => '1005',
+            ],
+            [
+                'id'=>'32',
+                'rol_id'=>'10',
+                'fase_id' => '401',
+                'requisito_id' => '1006',
+            ],
+            [
+                'id'=>'33',
+                'rol_id'=>'10',
+                'fase_id' => '401',
+                'requisito_id' => '1007',
+            ],
+            [
+                'id'=>'34',
+                'rol_id'=>'10',
+                'fase_id' => '401',
+                'requisito_id' => '1008',
+            ],
+            [
+                'id'=>'35',
+                'rol_id'=>'10',
+                'fase_id' => '401',
+                'requisito_id' => '1009',
+            ],
+            [
+                'id'=>'36',
+                'rol_id'=>'10',
+                'fase_id' => '401',
+                'requisito_id' => '1010',
+            ],
+            [
+                'id'=>'37',
+                'rol_id'=>'10',
+                'fase_id' => '401',
+                'requisito_id' => '1011',
+            ],
+            [
+                'id'=>'38',
+                'rol_id'=>'10',
+                'fase_id' => '401',
+                'requisito_id' => '1012',
+            ],
+            [
+                'id'=>'39',
+                'rol_id'=>'10',
+                'fase_id' => '401',
+                'requisito_id' => '1013',
+            ],
+            [
+                'id'=>'40',
+                'rol_id'=>'10',
+                'fase_id' => '401',
+                'requisito_id' => '1014',
+            ],
+            [
+                'id'=>'41',
+                'rol_id'=>'10',
+                'fase_id' => '401',
+                'requisito_id' => '1015',
+            ],
+            [
+                'id'=>'42',
+                'rol_id'=>'10',
+                'fase_id' => '401',
+                'requisito_id' => '1016',
+            ],
+            [
+                'id'=>'43',
+                'rol_id'=>'10',
+                'fase_id' => '401',
+                'requisito_id' => '1017',
+            ],
+            [
+                'id'=>'44',
+                'rol_id'=>'10',
+                'fase_id' => '401',
+                'requisito_id' => '1018',
+            ],
+            [
+                'id'=>'45',
+                'rol_id'=>'10',
+                'fase_id' => '401',
+                'requisito_id' => '1019',
+            ],
+            [
+                'id'=>'46',
+                'rol_id'=>'10',
+                'fase_id' => '401',
+                'requisito_id' => '1020',
+            ],
+            [
+                'id'=>'47',
+                'rol_id'=>'10',
+                'fase_id' => '401',
+                'requisito_id' => '1021',
+            ],
+            [
+                'id'=>'48',
+                'rol_id'=>'10',
+                'fase_id' => '401',
+                'requisito_id' => '1022',
+            ],
+           //resolucion de expedito y consejo de facultad
+            [                
+                'id'=>'49',
+                'rol_id'=>'5',
+                'fase_id' => '402',
+                'requisito_id' => '1023',
+            ],
+            [
+                'id'=>'50',
+                'rol_id'=>'5',
+                'fase_id' => '402',
+                'requisito_id' => '1024',
+            ],
+            [
+                'id'=>'51',
+                'rol_id'=>'5',
+                'fase_id' => '402',
+                'requisito_id' => '1025',
+            ],
+            //Vicerrectorado
+            [
+                'id'=>'52',
+                'rol_id'=>'11',
+                'fase_id' => '403',
+                'requisito_id' => '1026',
+            ],
+        //4 fin bachiller trabajo undac 
+        
+        //inicio titulo presencial    
+            //solicitudes para inscri
+            [
+                'id'=>'53',
+                'rol_id'=>'10',
+                'fase_id' => '1301',
+                'requisito_id' => '4001',
+            ],
+            [
+                'id'=>'54',
+                'rol_id'=>'10',
+                'fase_id' => '1301',
+                'requisito_id' => '4002',
+            ],
+            [
+                'id'=>'55',
+                'rol_id'=>'10',
+                'fase_id' => '1301',
+                'requisito_id' => '4003',
+            ],
+            [
+                'id'=>'56',
+                'rol_id'=>'10',
+                'fase_id' => '1301',
+                'requisito_id' => '4004',
+            ],
+            //'Designación de asesor'
+            [
+                'id'=>'57',
+                'rol_id'=>'5',
+                'fase_id' => '1302',
+                'requisito_id' => '4005',
+            ],
+            //Informe de originalidad
+            [
+                'id'=>'58',
+                'rol_id'=>'9',
+                'fase_id' => '1303',
+                'requisito_id' => '4006',
+            ],          
+            [
+                'id'=>'59',
+                'rol_id'=>'9',
+                'fase_id' => '1303',
+                'requisito_id' => '4007',
+            ],
+            //Resolución de inscripción 
+            [
+                'id'=>'60',
+                'rol_id'=>'5',
+                'fase_id' => '1304',
+                'requisito_id' => '4008',
+            ],
+            //Solicitud de aprobación de investigación
+            [
+                'id'=>'61',
+                'rol_id'=>'10',
+                'fase_id' => '1305',
+                'requisito_id' => '4009',
+            ],
+            //Informe aprobatorio(Borrador de tesi
+            [
+                'id'=>'62',
+                'rol_id'=>'9',
+                'fase_id' => '1306',
+                'requisito_id' => '4010',
+            ],
+            //solicitud para nombramiento de jurados
+            [
+                'id'=>'63',
+                'rol_id'=>'10',
+                'fase_id' => '1307',
+                'requisito_id' => '4011',
+            ],
+            //Nombramiento de jurados
+            [
+                'id'=>'64',
+                'rol_id'=>'5',
+                'fase_id' => '1308',
+                'requisito_id' => '4012',
+            ],
+            //Informe de jurados
+            [
+                'id'=>'65',
+                'rol_id'=>'8',
+                'fase_id' => '1309',
+                'requisito_id' => '4013',
+            ],
+            [
+                'id'=>'66',
+                'rol_id'=>'8',
+                'fase_id' => '1309',
+                'requisito_id' => '4014',
+            ],
+            [
+                'id'=>'67',
+                'rol_id'=>'8',
+                'fase_id' => '1309',
+                'requisito_id' => '4015',
+            ],
+            //Cargando solicitud para aprobación de tesis
+            [
+                'id'=>'68',
+                'rol_id'=>'10',
+                'fase_id' => '1310',
+                'requisito_id' => '4016',
+            ],
+            //Revision de tesis Aprobación
+            [
+                'id'=>'69',
+                'rol_id'=>'5',
+                'fase_id' => '1311',
+                'requisito_id' => '4017',
+            ],
+            //Solicitud para ser declarado expedito y completar requisitos
+            [
+                'id'=>'70',
+                'rol_id'=>'10',
+                'fase_id' => '1312',
+                'requisito_id' => '4018',
+            ],
+            [
+                'id'=>'71',
+                'rol_id'=>'10',
+                'fase_id' => '1312',
+                'requisito_id' => '4019',
+            ],
+            [
+                'id'=>'72',
+                'rol_id'=>'10',
+                'fase_id' => '1312',
+                'requisito_id' => '4020',
+            ],
+            [
+                'id'=>'73',
+                'rol_id'=>'10',
+                'fase_id' => '1312',
+                'requisito_id' => '4021',
+            ],
+            [
+                'id'=>'74',
+                'rol_id'=>'10',
+                'fase_id' => '1312',
+                'requisito_id' => '4022',
+            ],
+            [
+                'id'=>'75',
+                'rol_id'=>'10',
+                'fase_id' => '1312',
+                'requisito_id' => '4023',
+            ],
+            [
+                'id'=>'76',
+                'rol_id'=>'10',
+                'fase_id' => '1312',
+                'requisito_id' => '4024',
+            ],
+            [
+                'id'=>'77',
+                'rol_id'=>'10',
+                'fase_id' => '1312',
+                'requisito_id' => '4025',
+            ],
+            [
+                'id'=>'78',
+                'rol_id'=>'10',
+                'fase_id' => '1312',
+                'requisito_id' => '4026',
+            ],
+            [
+                'id'=>'79',
+                'rol_id'=>'10',
+                'fase_id' => '1312',
+                'requisito_id' => '4027',
+            ],
+            [
+                'id'=>'80',
+                'rol_id'=>'10',
+                'fase_id' => '1312',
+                'requisito_id' => '4028',
+            ],
+            [
+                'id'=>'81',
+                'rol_id'=>'10',
+                'fase_id' => '1312',
+                'requisito_id' => '4029',
+            ],
+            //Expediente para expedito
+            [
+                'id'=>'82',
+                'rol_id'=>'5',
+                'fase_id' => '1313',
+                'requisito_id' => '4030',
+            ],
+            //Solicitud de fecha y hora de suste
+            [
+                'id'=>'83',
+                'rol_id'=>'10',
+                'fase_id' => '1314',
+                'requisito_id' => '4031',
+            ],
+            //Resolución de fecha y hora de sustentacion
+            [
+                'id'=>'84',
+                'rol_id'=>'5',
+                'fase_id' => '1315',
+                'requisito_id' => '4032',
+            ],
+            //Solicitud para aprobación de título profesio
+            [
+                'id'=>'85',
+                'rol_id'=>'10',
+                'fase_id' => '1316',
+                'requisito_id' => '4033',
+            ],
+            [
+                'id'=>'86',
+                'rol_id'=>'10',
+                'fase_id' => '1316',
+                'requisito_id' => '4034',
+            ],
+            [
+                'id'=>'87',
+                'rol_id'=>'10',
+                'fase_id' => '1316',
+                'requisito_id' => '4035',
+            ],
+            [
+                'id'=>'88',
+                'rol_id'=>'10',
+                'fase_id' => '1316',
+                'requisito_id' => '4036',
+            ],
+            [
+                'id'=>'89',
+                'rol_id'=>'10',
+                'fase_id' => '1316',
+                'requisito_id' => '4037',
+            ],
+            //Revisión en consejo de facultad
+            [
+                'id'=>'90',
+                'rol_id'=>'5',
+                'fase_id' => '1317',
+                'requisito_id' => '4038',
+            ],
+            [
+                'id'=>'91',
+                'rol_id'=>'5',
+                'fase_id' => '1317',
+                'requisito_id' => '4039',
+            ],
+            //Vicerrectorado
+            [
+                'id'=>'92',
+                'rol_id'=>'11',
+                'fase_id' => '1318',
+                'requisito_id' => '4040',
+            ],
+        //fin titulo automatico
+
+
+
+        
         ]);
     }
 }
